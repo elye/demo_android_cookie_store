2fd548b0885f65e8e5238038b3b0dee08dbd7a89
elye <elye.project@gmail.com>
2022-06-25 20:05:38 +1000

    Upgrade version

--- base-module/src/main/java/com/elyeproj/basemodule/WebviewActivity.kt
+++ base-module/src/main/java/com/elyeproj/basemodule/WebviewActivity.kt
@@ -1,7 +1,7 @@
 package com.elyeproj.basemodule
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_webview.*
 
@@ -22,11 +22,9 @@ class WebviewActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
+    override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        if (outState != null) {
-            webview_main.saveState(outState)
-        }
+        webview_main.saveState(outState)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
